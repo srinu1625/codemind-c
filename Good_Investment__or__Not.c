c7698b8de135cfc6f7d55f9e1ea6742e07c3595a
srinu1625 <153898551+srinu1625@users.noreply.github.com>
2024-02-13 10:34:47 +0530

    #include<stdio.h> int main() {     int x,d,ans;     scanf("%d%d",&x,&d);
        if(x>=2*d)
        {
            printf("YES");
        }
        else
        {
            printf("NO");
        }
    }

---
+++ Good_Investment__or__Not.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,d,ans;
+    scanf("%d%d",&x,&d);
+
+    if(x>=2*d)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
